4e0c8cc521703662bac32b83c355a0f3829209fb
Seamoon Pandey <hackedasocial@gmail.com>
2023-11-09 18:25:28 +0545

    add instance for the solution test

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -7,3 +7,14 @@ class Solution:
             else:
                 hashset.add(n)
         return False
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Test the containDuplicate method with a list of integers
+nums = [1, 2, 3, 4, 5]
+result = solution.containDuplicate(nums)
+if result:
+    print("There are duplicate elements in the list.")
+else:
+    print("There are no duplicate elements in the list.")
